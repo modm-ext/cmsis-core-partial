458118995c21dfce70b1bc8f5a4d791138cb236f
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2024-03-25 22:15:20 +0100

    Update to CMSIS 6

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# CMSIS_5 partial tree
+# CMSIS core partial tree
 
-This is just a partial copy of the CMSIS_5 repository to reduce repository size.
+This is just a partial copy of the CMSIS core repository to reduce repository size.
 The following paths are extracted:
 
 - `CMSIS/Core/Include`
-- `CMSIS/LICENSE.txt`
+- `CMSIS/LICENSE`
 
 This repository is updated periodically by GitHub Actions.
--- update.py
+++ update.py
@@ -7,23 +7,25 @@ import urllib.request
 
 source_paths = [
     "CMSIS/Core/Include/**/*.h",
-    "LICENSE.txt",
+    "LICENSE",
 ]
 
-with urllib.request.urlopen("https://api.github.com/repos/arm-software/CMSIS_5/releases/latest") as response:
+with urllib.request.urlopen("https://api.github.com/repos/arm-software/CMSIS_6/releases/latest") as response:
    tag = json.loads(response.read())["tag_name"]
 
 # clone the repository
 if "--fast" not in sys.argv:
-    print("Cloning CMSIS_5 repository at tag v{}...".format(tag))
+    print("Cloning CMSIS_6 repository at tag {}...".format(tag))
     shutil.rmtree("cmsis_src", ignore_errors=True)
     subprocess.run("GIT_LFS_SKIP_SMUDGE=1 git clone --depth=1 --branch {} ".format(tag) +
-                   "https://github.com/arm-software/CMSIS_5.git cmsis_src", shell=True)
+                   "https://github.com/arm-software/CMSIS_6.git cmsis_src", shell=True)
 
 # remove the sources in this repo
 shutil.rmtree("CMSIS", ignore_errors=True)
+Path("LICENSE.txt").unlink(missing_ok=True)
+Path("LICENSE").unlink(missing_ok=True)
 
-print("Copying CMSIS_5 sources...")
+print("Copying CMSIS_6 sources...")
 for pattern in source_paths:
     for path in Path("cmsis_src").glob(pattern):
         if not path.is_file(): continue
@@ -35,6 +37,6 @@ for pattern in source_paths:
                            dest.open("w", encoding="utf-8") as wfile:
             wfile.writelines(l.rstrip()+"\n" for l in rfile.readlines())
 
-subprocess.run("git add CMSIS LICENSE.txt", shell=True)
+subprocess.run("git add CMSIS LICENSE", shell=True)
 if subprocess.call("git diff-index --quiet HEAD --", shell=True):
-    subprocess.run('git commit -m "Update CMSIS to v{}"'.format(tag), shell=True)
+    subprocess.run('git commit -m "Update CMSIS to {}"'.format(tag), shell=True)
